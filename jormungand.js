3122aa7c316ecadf785fc14514d5a15c5fb9821b
igorrcosta <igor.bioinfo@gmal.com>
2016-03-01 18:27:37 -0300

    Game over msg.

--- jormungand.js
+++ jormungand.js
@@ -9,15 +9,21 @@ function preload() {
 
 }
 
-var snakeHead; //head of snake sprite
-var snakeSection = new Array(); //array of sprites that make the snake body sections
-var snakePath = new Array(); //arrary of positions(points) that have to be stored for the path the sections follow
-var numSnakeSections = 3; //number of snake body sections
-var scaleX = 0.7;
-var scaleY = 0.7;
-var speed = 5;
-var snakeSpacer = 5; //parameter that sets the spacing between sections
-//var n = 1;
+var snakeHead;
+var snakeSection = new Array();
+var snakePath = new Array();
+var numSnakeSections = 10; //1-3000
+var scaleX = 0.3; //0.3 - 1
+var scaleY = scaleX;
+var speed = 5; //2-5
+var ang_speed = 200 //200-600
+var snakeSpacer = 2; //5-15
+//scale / speed / spacer
+//	 /  2 /  3 / 4 / 5
+//1.0/ 15 / 10 / 7 / 6
+//0.7/ 12 /  8 / 6 / 5
+//0.5/  8 /  5 / 4 / 3
+//0.3/  4 /  3 / 3 / 2
 
 function create() {
 
@@ -31,9 +37,11 @@ function create() {
     snakeHead = game.add.sprite(400, 300, 'head_s');
     snakeHead.scale.setTo(scaleX,scaleY);
     snakeHead.anchor.setTo(0.5, 0.5);
-
+	snakeHead.outOfBoundsKill = true;
+	snakeHead.checkWorldBounds = true;
+	
     game.physics.enable(snakeHead, Phaser.Physics.ARCADE);
-    
+    snakeHead.events.onOutOfBounds.add(killSnake, this);
     //  Init snakeSection array
     for (var i = 1; i <= numSnakeSections-1; i++)
     {
@@ -50,7 +58,7 @@ function create() {
     }
 	snakeHead.bringToTop();
 	game.camera.follow(snakeHead, Phaser.Camera.FOLLOW_LOCKON);
-	snakeHead.body.collideWorldBounds = true;
+	//snakeHead.body.collideWorldBounds = true
 	
 
 }
@@ -85,11 +93,11 @@ function update() {
 	}
     if (cursors.left.isDown)
     {
-        snakeHead.body.angularVelocity = -300;
+        snakeHead.body.angularVelocity = - ang_speed;
     }
     else if (cursors.right.isDown)
     {
-        snakeHead.body.angularVelocity = 300;
+        snakeHead.body.angularVelocity = ang_speed;
     }
 	else
 	{
@@ -97,6 +105,23 @@ function update() {
 	}
 	//n += 1
 
+}
+
+function killSnake(){
+	var style = { font: "65px Arial", fill: "#ffffff", align: "center" };
+	var gameOver = game.add.text(game.world.centerX, game.world.centerY, "Game Over", style);
+	gameOver.anchor.setTo(0.5, 0.5);
+	for (var i = 1; i <= numSnakeSections-1; i++)
+	{
+		snakeSection[i].kill();
+	}
+	if (game.camera.target)
+	{
+		game.camera.follow(null);
+	}
+	game.add.tween(game.camera).to({x: gameOver.x - (game.camera.width / 2),
+									y: gameOver.y - (game.camera.height / 2) },
+									750, Phaser.Easing.Quadratic.InOut, true);
 }
 
 function render() {
