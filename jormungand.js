aa4cb8c8d85fdaa835c4ca8b1ea9067154fa0475
igorrcosta <igor.bioinfo@gmal.com>
2016-03-02 15:11:47 -0300

    Better world bounds death.

--- jormungand.js
+++ jormungand.js
@@ -6,18 +6,17 @@ function preload() {
 	game.load.image('head_s','assets/circle_head_small.png');
     game.load.image('body_s','assets/circle_small.png');
 	game.load.image('tile','assets/tile.png');
-
 }
 
 var snakeHead;
 var snakeSection = new Array();
 var snakePath = new Array();
 var numSnakeSections = 10; //1-3000
-var scaleX = 0.3; //0.3 - 1
+var scaleX = 0.5; //0.3 - 1
 var scaleY = scaleX;
-var speed = 5; //2-5
+var speed = 3; //2-5
 var ang_speed = 200 //200-600
-var snakeSpacer = 2; //5-15
+var snakeSpacer = 5; //5-15
 //scale / speed / spacer
 //	 /  2 /  3 / 4 / 5
 //1.0/ 15 / 10 / 7 / 6
@@ -28,21 +27,17 @@ var snakeSpacer = 2; //5-15
 function create() {
 
     game.physics.startSystem(Phaser.Physics.ARCADE);
-
     //game.world.setBounds(0, 0, 800, 600);
 	game.world.setBounds(0, 0, 2400, 1800);
 	tilesprite = game.add.tileSprite(0, 0, 2400, 1800, 'tile');
     cursors = game.input.keyboard.createCursorKeys();
 
-    snakeHead = game.add.sprite(400, 300, 'head_s');
+    snakeHead = game.add.sprite(game.world.centerX, game.world.centerY, 'head_s');
     snakeHead.scale.setTo(scaleX,scaleY);
     snakeHead.anchor.setTo(0.5, 0.5);
-	snakeHead.outOfBoundsKill = true;
-	snakeHead.checkWorldBounds = true;
-	
     game.physics.enable(snakeHead, Phaser.Physics.ARCADE);
-    snakeHead.events.onOutOfBounds.add(killSnake, this);
-    //  Init snakeSection array
+	game.camera.follow(snakeHead, Phaser.Camera.FOLLOW_LOCKON);
+	
     for (var i = 1; i <= numSnakeSections-1; i++)
     {
         snakeSection[i] = game.add.sprite(400+(snakeHead.height*i), 300, 'body_s');
@@ -50,78 +45,81 @@ function create() {
         snakeSection[i].anchor.setTo(0.5, 0.5);
 		//game.physics.enable(snakeSection[i], Phaser.Physics.ARCADE);
     }
-    
-    //  Init snakePath array
     for (var i = 0; i <= snakeSpacer*numSnakeSections; i++)
     {
         snakePath[i] = new Phaser.Point(400, 300);
     }
-	snakeHead.bringToTop();
-	game.camera.follow(snakeHead, Phaser.Camera.FOLLOW_LOCKON);
-	//snakeHead.body.collideWorldBounds = true
 	
-
+	snakeHead.bringToTop();
 }
 
 function update() {
+	if (snakeHead.alive)
+	{
+		moveHead();	
+		moveBody();
+		if (cursors.left.isDown)
+		{
+			snakeHead.body.angularVelocity = - ang_speed;
+		}
+		else if (cursors.right.isDown)
+		{
+			snakeHead.body.angularVelocity = ang_speed;
+		}
+		else
+		{
+			snakeHead.body.angularVelocity = 0;
+		}
+		checkDeath();
+	}
+}
 
-	//speed += 1;
-	
-	// Everytime the snake head moves, insert the new location at the start of the array, 
-	// and knock the last position off the end
-
-	//n = 1;
+function moveHead() {
 	var angle = ((Math.PI*2)*snakeHead.angle)/360;
 	snakeHead.x += speed*Math.cos(angle);
 	snakeHead.y += speed*Math.sin(angle);
-	var part = snakePath.pop();
-	
-	part.setTo(snakeHead.x, snakeHead.y);
+}
 
+function moveBody() {
+	// Everytime the snake head moves, insert the new location at the start of the array, 
+	// and knock the last position off the end
+	var part = snakePath.pop();
+	part.setTo(snakeHead.x, snakeHead.y);
 	snakePath.unshift(part);
-	//game.physics.arcade.moveToObject(snakeSection[1], snakeHead, speed*60);
-	//console.log(snakeSection[1].y, snakeSection[2].y);
 	for (var i = 1; i <= numSnakeSections - 1; i++)
 	{
-		//section = snakeSection[i];
-		//previous = snakeSection[i-1];
-		//console.log(i);
-		//console.log(section.y, previous.velocity);
-		//game.physics.arcade.moveToObject(section, previous, speed*60)
 		snakeSection[i].x = snakePath[i*snakeSpacer].x;
 		snakeSection[i].y = snakePath[i*snakeSpacer].y;
 	}
-    if (cursors.left.isDown)
-    {
-        snakeHead.body.angularVelocity = - ang_speed;
-    }
-    else if (cursors.right.isDown)
-    {
-        snakeHead.body.angularVelocity = ang_speed;
-    }
-	else
-	{
-	    snakeHead.body.angularVelocity = 0;
-	}
-	//n += 1
+}
 
+function checkDeath() {
+	// Check world bounds death
+	if ((snakeHead.y > game.world.bounds.height - snakeHead.height/2) ||
+		(snakeHead.y < 0 + snakeHead.height/2) ||
+		(snakeHead.x > game.world.bounds.width - snakeHead.width/2) ||
+		(snakeHead.x < 0 + snakeHead.width/2))
+	{
+		killSnake();
+	}
 }
 
 function killSnake(){
-	var style = { font: "65px Arial", fill: "#ffffff", align: "center" };
-	var gameOver = game.add.text(game.world.centerX, game.world.centerY, "Game Over", style);
-	gameOver.anchor.setTo(0.5, 0.5);
-	for (var i = 1; i <= numSnakeSections-1; i++)
-	{
-		snakeSection[i].kill();
-	}
 	if (game.camera.target)
 	{
 		game.camera.follow(null);
 	}
+	for (var i = 1; i <= numSnakeSections-1; i++)
+	{
+		snakeSection[i].kill();
+	}
+	var style = { font: "65px Arial", fill: "#ffffff", align: "center" };
+	var gameOver = game.add.text(game.world.centerX, game.world.centerY, "Game Over", style);
+	gameOver.anchor.setTo(0.5, 0.5);
 	game.add.tween(game.camera).to({x: gameOver.x - (game.camera.width / 2),
 									y: gameOver.y - (game.camera.height / 2) },
 									750, Phaser.Easing.Quadratic.InOut, true);
+	snakeHead.kill();
 }
 
 function render() {
