d90217559990e65567443c07a8c4a64beb9bba19
igorrcosta <igor.bioinfo@gmal.com>
2016-03-03 16:39:50 -0300

    Accepts touch control.

--- jormungand.js
+++ jormungand.js
@@ -3,11 +3,15 @@ var game = new Phaser.Game(600, 600, Phaser.CANVAS, 'jormungand', { preload: pre
 function preload() {
     //game.load.image('head','assets/circle_head.png');
     //game.load.image('body','assets/circle.png');
-	game.load.image('head_s','assets/circle_head_small.png');
-    game.load.image('body_s','assets/circle_small.png');
+	game.load.image('head_s','assets/square_head_small.png');
+    game.load.image('body_s','assets/square_small.png');
+	//game.load.image('head_s','assets/circle_head_small.png');
+    //game.load.image('body_s','assets/circle_small.png');
 	game.load.image('tile','assets/tile.png');
 	game.load.image('food','assets/food.png');
 }
+
+var RIGHT = 0, LEFT = 1;
 
 var snakeHead;
 var snakeSection = new Array();
@@ -16,9 +20,11 @@ var numSnakeSections = 10; //1-3000
 var scaleX = 0.5; //0.3 - 1
 var scaleY = scaleX;
 var speed = 3; //2-5
-var ang_speed = 200 //200-600
-var snakeSpacer = 5; //5-15
-var maxSnakeSections = 1000;
+var ang_speed = 300 //200-600
+var snakeSpacer = 7; //5-15
+var maxSnakeSections = 1500;
+var startingFood = 50;
+var n = 0;
 //scale / speed / spacer
 //	 /  2 /  3 / 4 / 5
 //1.0/ 15 / 10 / 7 / 6
@@ -29,8 +35,8 @@ var maxSnakeSections = 1000;
 function create() {
 	
     game.physics.startSystem(Phaser.Physics.ARCADE);
-    game.world.setBounds(0, 0, 600, 600);
-	//game.world.setBounds(0, 0, 2400, 1800);
+    //game.world.setBounds(0, 0, 600, 600);
+	game.world.setBounds(0, 0, 2400, 1800);
 	tilesprite = game.add.tileSprite(0, 0, 2400, 1800, 'tile');
     cursors = game.input.keyboard.createCursorKeys();
 
@@ -39,21 +45,25 @@ function create() {
     snakeHead.anchor.setTo(0.5, 0.5);
     game.physics.enable(snakeHead, Phaser.Physics.ARCADE);
 	game.camera.follow(snakeHead, Phaser.Camera.FOLLOW_LOCKON);
-	//snakeBody = game.add.group();
+	snakeBody = game.add.group();
 
     for (var i = 0; i <= numSnakeSections; i++)
     {
-        snakeSection[i] = game.add.sprite(snakeHead.x, snakeHead.y, 'body_s');
-        snakeSection[i].scale.setTo(scaleX,scaleY);
-        snakeSection[i].anchor.setTo(0.5, 0.5);
-		game.physics.enable(snakeSection[i], Phaser.Physics.ARCADE);
+		snakeBody.create(snakeHead.x, snakeHead.y, 'body_s')
+        //snakeSection[i] = game.add.sprite(snakeHead.x, snakeHead.y, 'body_s');
+        snakeBody.children[i].scale.setTo(scaleX,scaleY);
+        snakeBody.children[i].anchor.setTo(0.5, 0.5);
+		if (i) //Skip the first body
+		{
+			game.physics.enable(snakeBody.children[i], Phaser.Physics.ARCADE);
+		}
     }
     for (var i = 0; i <= snakeSpacer*maxSnakeSections; i++)
     {
-        snakePath[i] = new Phaser.Point(snakeHead.x, snakeHead.y);
+        snakePath[i] = [new Phaser.Point(snakeHead.x, snakeHead.y), snakeHead.angle];
     }
 	foodPool = game.add.group();
-	createFood(1);
+	createFood(startingFood);
 	snakeHead.bringToTop();
 }
 
@@ -61,6 +71,11 @@ function update() {
 	if (snakeHead.alive)
 	{
 		game.physics.arcade.overlap(snakeHead, foodPool, eat);
+		if (n > 10)
+		{
+			game.physics.arcade.overlap(snakeHead, snakeBody, killSnake);
+		}
+		
 		moveHead();	
 		moveBody();
 		if (cursors.left.isDown)
@@ -71,11 +86,23 @@ function update() {
 		{
 			snakeHead.body.angularVelocity = ang_speed;
 		}
+		else if (game.input.pointer1.isDown)
+		{
+			if (Math.floor(game.input.x/(game.width/2)) === LEFT)
+			{
+				snakeHead.body.angularVelocity = ang_speed;
+			}    
+			else if (Math.floor(game.input.x/(game.width/2)) === RIGHT)
+			{
+				snakeHead.body.angularVelocity = - ang_speed;
+			}
+		}
 		else
 		{
 			snakeHead.body.angularVelocity = 0;
 		}
 		checkDeath();
+		n++
 	}
 }
 
@@ -86,8 +113,8 @@ function pass() {
 function createFood(n) {
 	for (var i = 0; i < n; i++)
 	{
-		randomX = Math.floor((Math.random() * game.world.width));
-		randomY = Math.floor((Math.random() * game.world.height));
+		randomX = game.rnd.integerInRange(10, game.world.width-10);
+		randomY = game.rnd.integerInRange(10, game.world.height-10);
 		foodPool.create(randomX, randomY, 'food');
 		console.log(randomX, randomY, i);
 	}
@@ -106,9 +133,11 @@ function eat(head, food) {
 function snakeGrow() {
 	numSnakeSections++;
 	//console.log(numSnakeSections, numSnakeSections*snakeSpacer);
-	snakeSection[numSnakeSections] = game.add.sprite(snakePath[numSnakeSections*snakeSpacer].x, snakePath[numSnakeSections*snakeSpacer].y, 'body_s');
-	snakeSection[numSnakeSections].scale.setTo(scaleX,scaleY);
-    snakeSection[numSnakeSections].anchor.setTo(0.5, 0.5);
+	snakeBody.create(snakePath[numSnakeSections*snakeSpacer][0].x, snakePath[numSnakeSections*snakeSpacer][0].y, 'body_s');
+	game.physics.enable(snakeBody.children[numSnakeSections], Phaser.Physics.ARCADE);
+	snakeBody.children[numSnakeSections].scale.setTo(scaleX,scaleY);
+    snakeBody.children[numSnakeSections].anchor.setTo(0.5, 0.5);
+	snakeBody.children[numSnakeSections].angle = snakePath[numSnakeSections*snakeSpacer][1];
 }
 	
 function moveHead() {
@@ -121,12 +150,14 @@ function moveBody() {
 	// Everytime the snake head moves, insert the new location at the start of the array, 
 	// and knock the last position off the end
 	var part = snakePath.pop();
-	part.setTo(snakeHead.x, snakeHead.y);
+	part[0].setTo(snakeHead.x, snakeHead.y);
+	part[1] = snakeHead.angle;
 	snakePath.unshift(part);
 	for (var i = 0; i <= numSnakeSections; i++)
 	{
-		snakeSection[i].x = snakePath[i*snakeSpacer].x;
-		snakeSection[i].y = snakePath[i*snakeSpacer].y;
+		snakeBody.children[i].x = snakePath[(i*snakeSpacer)+snakeSpacer][0].x;
+		snakeBody.children[i].y = snakePath[(i*snakeSpacer)+snakeSpacer][0].y;
+		snakeBody.children[i].angle = snakePath[(i*snakeSpacer)+snakeSpacer][1];
 	}
 }
 
@@ -148,7 +179,7 @@ function killSnake(){
 	}
 	for (var i = 0; i <= numSnakeSections; i++)
 	{
-		snakeSection[i].kill();
+		snakeBody.children[i].kill();
 	}
 	var style = { font: "65px Arial", fill: "#ffffff", align: "center" };
 	var gameOver = game.add.text(game.world.centerX, game.world.centerY, "Game Over", style);
